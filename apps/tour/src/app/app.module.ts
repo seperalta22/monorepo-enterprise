ec0cdb3f3339106ffcc7d23221b8a683529825a4
Sergio Peralta <seperalta88@gmail.com>
2022-09-24 01:02:59 -0300

    first commit

--- apps/tour/src/app/app.module.ts
+++ apps/tour/src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path: 'admin',
+        loadChildren: () => import('@packt/admin').then((m) => m.AdminModule),
+      },
+      {
+        path: 'tour',
+        loadChildren: () =>
+          import('@packt/visitor').then((m) => m.VisitorModule),
+      },
+      { path: '', pathMatch: 'full', redirectTo: 'tour' },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
